9ef24bdd885d4ef2ccb09e95ca71a87b585f5219
jrgustmadtown <jrgust@wisc.edu>
2025-11-03 10:09:26 -0600

    start

---
+++ FF.py
@@ -0,0 +1,16 @@
+import sys
+
+inputy = list(map(int, sys.stdin.buffer.read().split()))
+k = inputy[0]
+index = 1
+
+for _ in range(k):
+    n = inputy[index]
+    index += 1
+  
+    sol = 1
+    print(str(sol))
+    
+
+
+
